319598b80751f870a786714d7b9cd4e91c55dbc6
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2012-12-29 23:11:47 -0700

    regex needs to be static since we don't use any instances

--- Media Modules/MediaImgur.m
+++ Media Modules/MediaImgur.m
@@ -10,7 +10,7 @@
 
 @implementation MediaImgur
 
-NSRegularExpression *regex = nil;
+static NSRegularExpression *regex = nil;
 
 + (void)load {
   regex = [NSRegularExpression regularExpressionWithPattern:@"^https?://(?:[i.]|[edge.]|[www.])*imgur.com/(?:r/[\\w]+/)?([\\w]{5,}(?:[&,][\\w]{5,})?)(\\.[\\w]{3,4})?(?:#(\\d*))?(?:\\?(?:\\d*))?$" options:NSRegularExpressionCaseInsensitive error:nil];
@@ -26,6 +26,8 @@ NSRegularExpression *regex = nil;
   NSString *href = [node getAttributeNamed:@"href"];
   NSTextCheckingResult *match = [regex firstMatchInString:href options:0 range:NSMakeRange(0, [href length])];
   NSString *group = [href substringWithRange:[match rangeAtIndex:1]];
+  
+  // For now we want to ignore any album style links, just single image pages...
   if ([group rangeOfString:@"&"].location == NSNotFound && [group rangeOfString:@","].location == NSNotFound) {
     [MediaModuleFactory removeChildNodes:node->_node];
     [MediaModuleFactory imageAtNode:node->_node src:[NSString stringWithFormat:@"http://i.imgur.com/%@.jpg", group]];
